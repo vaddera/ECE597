db0c3375772760676019736c5af1e837929284af
vaddera <vaddera@rose-hulman.edu>
2014-11-10 22:55:27 +0000

    IR sensors reads up to 8cm now.

--- Robot/IRread.py
+++ Robot/IRread.py
@@ -1,26 +1,58 @@
 import Adafruit_BBIO.ADC as ADC #Similar to analog read
 import time as T
 
-# Voltage Multiplier:
-Volt = 1.8
+# Useful Lists:
+IR1list = []
+IR2list = []
+IR3list = []
+IR4list = []
+IR5list = []
 
-# Setting previous read:
-prevA = 0
-prevB = 0
+# General purpose variables:
+count = 0
+samples = 20
+mult = 20
 
 ADC.setup() #may need to put argument
 
 while True:
 
-	#OUTA = ADC.read_raw("P9_37")
-	#OUTB = ADC.read_raw("P9_39")
-
-	# Another option:
+	# Reading analog inputs:
 	IR1 = ADC.read("P9_40")
 	IR2 = ADC.read("P9_39")
 	IR3 = ADC.read("P9_38")
 	IR4 = ADC.read("P9_37")
-	IR5 = ADC.read("p9_33")
+	IR5 = ADC.read("P9_33")
+
+	for i in range(samples):
+		count = count + 1
+
+		IR1list.append(round(IR1*mult,1))
+		IR2list.append(IR2)
+		IR3list.append(IR3)
+		IR4list.append(IR4)
+		IR5list.append(IR5)
+
+		if (count == samples):
+			# Calculating the average of 3 readings:
+			avgIR1 = sum(IR1list) / len(IR1list)
+			avgIR2 = sum(IR2list) / len(IR2list)
+			avgIR3 = sum(IR3list) / len(IR3list)
+			avgIR4 = sum(IR4list) / len(IR4list)
+			avgIR5 = sum(IR5list) / len(IR5list)
+			
+			# Clearing each list:
+			IR1list = []
+			IR2list = []
+			IR3list = []
+			IR4list = []
+			IR5list = []
+			count = 0
+
+	#print(round(avgIR1*mult,1))
+	print(avgIR1)
+
+	T.sleep(1)
 	
 	#OUTA = "%.3f" % OUTA
 	#OUTB = "%.3f" % OUTB
@@ -37,9 +69,9 @@ while True:
 	prevB = OUTB
 
 	T.sleep(5)
-
 '''
-print('IR1: ' + str(IR1))
+
+#print('IR1: ' + str(IR1))
 #print(IR1)
 #print('IR2 ')
 #print(OUTB)
